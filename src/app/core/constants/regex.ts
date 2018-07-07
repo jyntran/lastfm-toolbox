078e74bccd855d341cb5a9cf7671e3c9ea837c40
J. Tran <jyntran@gmail.com>
2018-07-07 01:52:03 -0400

    modify regex

--- src/app/core/constants/regex.ts
+++ src/app/core/constants/regex.ts
@@ -1,4 +1,4 @@
 export class Regex {
-	public static plaintext = /\s*\d{2}\:\d{2}\s*(\n?)/;
-	public static plaintextTrack = /^(\d+).\s*(.*)$/;
+	public static plaintext = /\s*\d{1,2}\:\d{2}\s*(\n?)/;
+	public static plaintextTrack = /^(\d+)\s*(?:.-?)\s*(.*)$/;
 }
